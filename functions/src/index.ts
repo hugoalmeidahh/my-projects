98792127c7f3b8cab3039c9346d87d9d3cea5024
Hugo Almeida <hugoalmeidahh@gmail.com>
2021-07-09 16:32:12 -0300

    first deploy app firebase

---
+++ functions/src/index.ts
@@ -0,0 +1,32 @@
+//import * as functions from "firebase-functions";
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+//import libraries
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
+
+//initialize firebase inorder to access its services
+admin.initializeApp(functions.config().firebase);
+
+//initialize express server
+const app = express();
+const main = express();
+
+//add the path to receive request and set json as bodyParser to process the body 
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+//initialize the database and the collection 
+const db = admin.firestore();
+const userCollection = 'users';
+
+//define google cloud function name
+export const webApi = functions.https.onRequest(main);
---
+++ functions/src/users.ts
@@ -0,0 +1,9 @@
+interface User {
+    firstName: String,
+    lastName: String,
+    email: String,
+    areaNumber: String,
+    department: String,
+    id:String,
+    contactNumber:String
+}
